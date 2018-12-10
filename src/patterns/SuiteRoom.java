dc1dfac8d93bbc8e7478d0c95b2fa8fe93afa77b
Brandon Nhem <brandonnhem@gmail.com>
2018-12-10 11:31:52 -0800

    Added JavaDocs

--- src/patterns/SuiteRoom.java
+++ src/patterns/SuiteRoom.java
@@ -1,5 +1,14 @@
 package patterns;
 
+/**
+*
+* @author Jamil Khan, Brandon Nhem, Anthony Pham, Brandon Walker
+* Purpose: The SuiteRoom creates a different MotelRoom type for the RoachMotel
+* Input: n/a
+* Output: getCost()
+*
+*/
+
 public class SuiteRoom extends MotelRoom {
 
 	private static final double COST = 100;
