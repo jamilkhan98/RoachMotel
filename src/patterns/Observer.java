757c450a7d483e1525ae98f423759faa7df376a7
BrandonWalker7 <45770701+BrandonWalker7@users.noreply.github.com>
2018-12-10 11:42:08 -0800

    Java docs

--- src/patterns/Observer.java
+++ src/patterns/Observer.java
@@ -1,5 +1,12 @@
 package patterns;
-
+/**
+*
+* @author Jamil Khan, Brandon Nhem, Anthony Pham, Brandon Walker
+* Purpose: 
+* Input: n/a
+* Output: n/a
+*
+*/
 public interface Observer {
     public void update(String name);
 }
