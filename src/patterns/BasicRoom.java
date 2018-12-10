baa93ed121f31634e9d8cf08d2aaa49f719f6553
jamilkhan98 <44854522+jamilkhan98@users.noreply.github.com>
2018-12-10 11:35:40 -0800

    Update BasicRoom.java

--- src/patterns/BasicRoom.java
+++ src/patterns/BasicRoom.java
@@ -1,5 +1,13 @@
 package patterns;
 
+/**
+*
+*@author Brandon Nhem, Jamil Khan, Anthony Pham, Brandon Walker
+*Purpose: to hold Roach Colonies
+*Input: none required
+*Outputs: description of room, and cost of room
+*/
+
 public class BasicRoom extends MotelRoom{
 	
 	private static final double COST = 50;
