c1d435f880476670fcdfaf685e0614512aea007f
Brandon Nhem <brandonnhem@gmail.com>
2018-12-10 11:37:28 -0800

    Added JavaDocs

--- src/patterns/Subject.java
+++ src/patterns/Subject.java
@@ -1,5 +1,14 @@
 package patterns;
 
+/**
+*
+* @author Jamil Khan, Brandon Nhem, Anthony Pham, Brandon Walker
+* Purpose: The Subject interface is meant for the RoachMotel and adds multiple Observers from the Observer 
+* Input: addObserver(), removeObserver()
+* Output: n/a
+*
+*/
+
 public interface Subject {
     public void addObserver(Observer o);
     public void removeObserver(Observer o);
