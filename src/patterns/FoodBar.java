06460584eebbb83e85edb5ae5a026549f9c9e618
jamilkhan98 <44854522+jamilkhan98@users.noreply.github.com>
2018-12-10 11:40:34 -0800

    added java docs

--- src/patterns/FoodBar.java
+++ src/patterns/FoodBar.java
@@ -1,5 +1,14 @@
 package patterns;
 
+/**
+*
+* @author Jamil Khan, Brandon Nhem, Anthony Pham, Brandon Walker
+* Purpose: To have FoodBar amenity added to Motel Room amenities
+* Input: Motel Room to which amenity will be added
+* Output: Motel Room to which amenity was added, the amenity, and total cost
+*
+*/
+
 public class FoodBar extends AmenitiesDecorator{
     MotelRoom mr;
     
