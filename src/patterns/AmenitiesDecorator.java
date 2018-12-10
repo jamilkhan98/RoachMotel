a7c9138bc214f6d90d9443a1c121459a2f12aeb3
jamilkhan98 <44854522+jamilkhan98@users.noreply.github.com>
2018-12-10 11:32:26 -0800

    added java docs

--- src/patterns/AmenitiesDecorator.java
+++ src/patterns/AmenitiesDecorator.java
@@ -1,5 +1,13 @@
 package patterns;
 
+/**
+*
+*@author Brandon Nhem, Jamil Khan, Anthony Pham, Brandon Walker
+*Purpose: Interface which allows Decorator Pattern to be used on Motel Room objects
+*Inputs: none required
+*Outputs: none required
+*/
+
 public abstract class AmenitiesDecorator extends MotelRoom{
     public abstract String getDescription();
 }
